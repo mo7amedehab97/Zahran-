ad4b07f694a8f9e4d7ef8d72c0483623a0d445a0
mo7amedehab97 <gazacityface@gmail.com>
2023-01-25 14:35:41 +0200

    upload genral rules and the main structur for the app

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import Banner from "../Components/Banner/Banner";
+import Navbar from "../Components/Navbar/Navbar";
+import Services from "../Components/Services/Services";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Services />
+    </>
+  );
+};
+
+export default Home;
